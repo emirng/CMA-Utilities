816715e6290a2a1c6d4a596621bca54e6639a80b
emirng <135670768+emirng@users.noreply.github.com>
2023-11-23 21:19:38 +0100

    fixed field-spell-error and this will make the field data actual being viewed. moved common used mock for camunda api to common setup. added test for view single process instance and its tasks and variables. fixed issue with test that didn't have a start path (it rely on previous test's path to work since the webdriver is not being reset between tests)

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -33,6 +33,25 @@ def setup(request, pytestconfig, httpserver):
     httpserver.expect_request('/v1/tasks/search').respond_with_json(items)
     # ---
 
+    # --- mock camunda api process-instances/search
+    keys = ['key', 'processVersion', 'bpmnProcessId', 'startDate', 'state', 'processDefinitionKey',
+            'tenantId', ]
+
+    items_values = [
+        [
+            '2251799813703087', '1', 'template-test', "2022-02-02T12:12:12.000+0000", 'ACTIVE',
+            '2251799813703085',  '<default>',
+        ],
+        [
+            '2251799813703081', '2', 'template-test-2', None, 'ACTIVE',
+            '2251799813703082',  '<default>',
+        ]
+    ]
+    items = [{k: v for k, v in zip(keys, values)} for values in items_values]
+    httpserver.expect_request(
+        '/v1/process-instances/search').respond_with_json({'items': items})
+    # ---
+
     app.config['AUTH_SERVER'], app.config['REST_SERVER'] = [
         'http://{0}:{1}'.format(httpserver.host, httpserver.port)] * 2
     app.config['DEBUG'] = pytestconfig.getoption('--cma-ws-debug')
--- tests/test_process_instance.py
+++ tests/test_process_instance.py
@@ -7,26 +7,6 @@ def test_process_instance_search_view(setup, webdriver):
         List process-instances at process-instance search view.
     """
 
-    # --- mock camunda api process-instances/search
-    httpserver = setup[1]
-    keys = ['key', 'processVersion', 'bpmnProcessId', 'startDate', 'state', 'processDefinitionKey',
-            'tenantId', ]
-
-    items_values = [
-        [
-            '2251799813703087', '1', 'template-test', "2022-02-02T12:12:12.000+0000", 'ACTIVE',
-            '2251799813703085',  '<default>',
-        ],
-        [
-            '2251799813703081', '2', 'template-test-2', None, 'ACTIVE',
-            '2251799813703082',  '<default>',
-        ]
-    ]
-    items = [{k: v for k, v in zip(keys, values)} for values in items_values]
-    httpserver.expect_request(
-        '/v1/process-instances/search').respond_with_json({'items': items})
-    # ---
-
     # get process instances
     webdriver.path_get('/process-instance')
 
@@ -50,11 +30,62 @@ def test_process_instance_search_view(setup, webdriver):
     assert actual == expected
 
 
+def test_view_single_process_instance_and_its_tasks_and_variables(setup, webdriver):
+    key = '2251799813703087'
+
+    # --- mock camunda api
+    httpserver = setup[1]
+
+    data = {
+        'key': key,
+        'processVersion': '1',
+        'bpmnProcessId': 'template-test',
+        'startDate': '2022-02-02\n12:12 +0000',
+        'state': 'ACTIVE',
+        'processDefinitionKey': '2251799813703085',
+        'tenantId': '<default>',
+    }
+
+    httpserver.expect_request(
+        f'/v1/process-instances/{key}').respond_with_json(data)
+
+    httpserver.expect_request(
+        '/v1/variables/search', method="POST", json={'processInstanceKey': key}).respond_with_json(
+        {
+            "items": [{
+                'key': '1',
+                'processInstanceKey': '2',
+                'scopeKey': '3',
+                'name': '4',
+                'value': '5',
+                'truncated': '6',
+                'tenantId': '7',
+            }]})
+    # ---
+
+    webdriver.path_get(f'/process-instance/{key}')
+
+    actual = list()
+    for table in webdriver.find_elements('xpath', "//table"):
+        inner_actual = list()
+        for tr in table.find_elements('xpath', './tbody/tr'):
+            item = [e.text for e in tr.find_elements('xpath', './th | ./td')]
+            inner_actual.append(item)
+        actual.append(inner_actual)
+
+    expected = [[['Key', '2251799813703087'], ['Process version', '1'], ['BPMN Process id', 'template-test'], ['Start date', '2022-02-02 12:12 +0000'], ['State', 'ACTIVE'], ['Process definition key', '2251799813703085'], ['Tenant id', '']], [['Id', 'Name', 'Creation data', 'Completion data', 'Assignee', 'Task state', 'Process definition key', 'Process instance key', 'Form', ''], ['6755399441084844', "Decide what's for dinner", '2022-02-02T12:12:12.000+0000',
+                                                                                                                                                                                                                                                                                                                                                                                               '2022-02-02T12:12:12.000+0000', 'Lisa', 'COMPLETED', '2251799813703085', '6755399441084839', 'Goto form', ''], ['6755399441084841', "Decide what's for dinner", '2022-02-02T12:12:12.000+0000', 'None', 'Unassigned\nAssign', 'CREATED', '2251799813703011', '6755399441084822', 'Goto form', 'Complete']], [['Key', 'Process instance key', 'Scope key', 'Name', 'Value', 'Truncated', 'Tenant id'], ['1', '2', '3', '4', '5', '6', '7']]]
+
+    assert actual == expected
+
+
 def test_process_instance_delete_at_search_view(setup, webdriver):
     """
         Make sure delete process-instances at process-instance search view actual sends
         the delete request to rest.
     """
+
+    webdriver.path_get('/process-instance')
 
     # --- mock camunda api process-instances/search
     httpserver = setup[1]
@@ -86,6 +117,7 @@ def test_process_instance_delete_at_search_view(setup, webdriver):
 
     # get process instances and delete second row button
     tr = find_table_row(webdriver, 'Key', pi_id)
+
     for delete_button in tr.find_elements('xpath', "./td/form/button[text()='Delete']"):
         delete_button.click()
 
